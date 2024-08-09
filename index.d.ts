62e3ab8617f23eca84bc7d726485742a95136b63
Nyanto <ola.mushroom@gmail.com>
2024-08-09 11:01:48 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 /**
  * @param replay A buffer containing the replay data.
+ * @param decode If true, also decode the replay.
  * @param parseInfoOnly If true, only parse info and skip replay data.
  * @returns An array containing the parsed replay data.
  * @remarks Reference: [.osr file format](https://osu.ppy.sh/wiki/en/Client/File_formats/osr_%28file_format%29)
  */
-export default function osr(replay: ArrayBufferLike | Buffer, parseInfoOnly: boolean): any[]
+export default function osr(replay: ArrayBufferLike | Buffer, decode: boolean, parseInfoOnly: boolean): any[]
--- index.js
+++ index.js
@@ -1,41 +1,50 @@
-import { decompress } from 'lzma'
 import { decodeUInt64 } from 'leb'
-const arr = []
+import { decompress } from 'lzma'
 let ptr = 0
+const arr = []
 const decoder = new TextDecoder('utf-8', {
   fatal: true
 })
-function parse() {
+function parseByte() {
   ptr++
   return arr[ptr - 1]
 }
 function parseLE(i) {
   ptr += i
-  return arr.slice(ptr - i, ptr).reduceRight((prev, current) => prev * 256 + current)
+  return arr
+    .slice(ptr - i, ptr)
+    .reduceRight((prev, current) => prev * 256 + current)
 }
 function parseStr() {
   if (arr[ptr] === 11) {
     ptr++
-    const leb128 = decodeUInt64(arr.slice(ptr, ptr + 8));
+    const leb128 = decodeUInt64(arr.slice(ptr, ptr + 8))
     const len = leb128.value
     ptr += len + leb128.nextIndex
     return decoder.decode(Uint8Array.from(arr.slice(ptr - len, ptr)))
   }
-  ptr++;
+  ptr++
   return ''
 }
-export default function osr(replay, parseInfoOnly) {
+function parseMods(n) {
+  if (n === 0) return n
+  const arr = []
+  for (let i = 0; i < 31; i++) if ((n & (1 << i)) >> i) arr.push(i)
+  if (arr.length > 1) return arr
+  return arr[0]
+}
+export default function parse(replay, decode, parseInfoOnly) {
+  const data = []
   ptr = 0
   arr.length = 0
-  for (const v of replay) arr.push(v);
-  const data = []
-  data.push(parse()) // Mode
+  for (const v of replay) arr.push(v)
+  data.push(parseByte()) // Mode
   data.push(parseLE(4)) // Version
-  for (let i = 0; i < 3; i++) data.push(parseStr()); // Name and hashes
-  for (let i = 0; i < 6; i++) data.push(parseLE(2)); // Hit counts
+  for (let i = 0; i < 3; i++) data.push(parseStr()) // Name and hashes
+  for (let i = 0; i < 6; i++) data.push(parseLE(2)) // Hit counts
   data.push(parseLE(4)) // Total score
   data.push(parseLE(2)) // Greatest combo
-  data.push(parse()) // Perfect/FC
+  data.push(parseByte()) // Perfect/FC
   data.push(parseLE(4)) // Mods
   data.push(parseStr()) // Life bar
   data.push(parseLE(8)) // Timestamp
@@ -44,6 +53,6 @@ export default function osr(replay, parseInfoOnly) {
   ptr += len
   if (!parseInfoOnly) data.push(decompress(arr.slice(ptr - len, ptr))) // Replay data
   data.push(parseLE(8)) // Score ID
-  if((ptr + 7) < arr.length) data.push(parseLE(8)) // Target practice
+  if (ptr + 7 < arr.length) data.push(parseLE(8)) // Target practice
   return data
 }
