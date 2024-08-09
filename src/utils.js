d0593fcacc8779f91c162cfa24c8df3203f08d5c
Nyanto <ola.mushroom@gmail.com>
2024-08-09 12:31:19 +0700

    update

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 export function decodeMods(n) {
   if (n === 0) return n
-  const arr = []
-  for (let i = 0; i < 31; i++) if ((n & (1 << i)) >> i) arr.push(i)
-  if (arr.length > 1) return arr
-  return arr[0]
+  const a = []
+  for (let i = 0; i < 31; i++) if ((n & (1 << i)) >> i) a.push(i)
+  if (a.includes(9)) {
+    a.splice(a.indexOf(6), 1) // Remove DT when NC exists
+  }
+  if (a.length > 1) return a
+  return a[0]
 }
