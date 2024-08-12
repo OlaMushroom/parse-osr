11a0f01e46224e9bd391723a4572ea45d8492069
Nyanto <ola.mushroom@gmail.com>
2024-08-12 21:01:47 +0700

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,7 +9,7 @@ export function parse(replay: ArrayBufferLike | Buffer, parseInfoOnly?: boolean)
  * @param options An object containing the decoding options.
  * @returns An array containing the decoded replay data.
  */
-export function decode(data: any[], options: {
+export function decode(data: any[], options?: {
   lifeBar?: boolean,
   mods?: boolean,
   replayData?: boolean,
@@ -19,4 +19,26 @@ export function decode(data: any[], options: {
  * @param data An array containing the parsed replay data.
  * @returns An object containing the parsed replay data.
 */
-export function objectify(data: any[]): object
+export function objectify(data: any[]): {
+  mode: number,
+  ver: number,
+  map: string,
+  name: string,
+  hash: string,
+  great: number,
+  ok: number,
+  meh: number,
+  geki: number,
+  katu: number,
+  miss: number,
+  score: number,
+  combo: number,
+  perfect: number,
+  mods: number | number[],
+  lifeBar: string | number[],
+  time: number | string,
+  length: number,
+  data?: string | number[][],
+  id?: number,
+  tp?: number
+}
